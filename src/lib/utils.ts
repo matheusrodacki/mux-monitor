902379026c51447c0df56feaca01912abc8502d4
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2025-03-07 23:43:23 -0300

    feat: :sparkles: Adiciona novos componentes e configurações, incluindo layout, ícones e gerenciamento de estado, além de remover arquivos desnecessários

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,9 +13,9 @@ export const focusInput = [
   // base
   "focus:ring-2",
   // ring color
-  "focus:ring-blue-200 focus:dark:ring-blue-700/30",
+  "focus:ring-indigo-200 focus:dark:ring-indigo-700/30",
   // border color
-  "focus:border-blue-500 focus:dark:border-blue-700",
+  "focus:border-indigo-500 focus:dark:border-indigo-700",
 ]
 
 // Tremor Raw focusRing [v0.0.1]
@@ -24,7 +24,7 @@ export const focusRing = [
   // base
   "outline outline-offset-2 outline-0 focus-visible:outline-2",
   // outline color
-  "outline-blue-500 dark:outline-blue-500",
+  "outline-indigo-500 dark:outline-indigo-500",
 ]
 
 // Tremor Raw hasErrorInput [v0.0.1]
@@ -37,3 +37,41 @@ export const hasErrorInput = [
   // ring color
   "ring-red-200 dark:ring-red-700/30",
 ]
+
+// Number formatter function
+
+export const usNumberformatter = (number: number, decimals = 0) =>
+  Intl.NumberFormat("us", {
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  })
+    .format(Number(number))
+    .toString()
+
+export const percentageFormatter = (number: number, decimals = 1) => {
+  const formattedNumber = new Intl.NumberFormat("en-US", {
+    style: "percent",
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(number)
+  const symbol = number > 0 && number !== Infinity ? "+" : ""
+
+  return `${symbol}${formattedNumber}`
+}
+
+export const millionFormatter = (number: number, decimals = 1) => {
+  const formattedNumber = new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    minimumFractionDigits: decimals,
+    maximumFractionDigits: decimals,
+  }).format(number)
+  return `${formattedNumber}M`
+}
+export const formatters: { [key: string]: any } = {
+  currency: (number: number, currency: string = "USD") =>
+    new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: currency,
+    }).format(number),
+  unit: (number: number) => `${usNumberformatter(number)}`,
+}
